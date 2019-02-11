e809ff42023e93666e780b670800fbc60d6f4dad
Patrick T Coakley <contact@patricktcoakley.com>
2019-02-10 22:29:33 -0600

    Optimize day 5 parts 1 and 2, bringing solution runtime down significantly.

--- main.cpp
+++ main.cpp
@@ -3,25 +3,18 @@
 //
 
 #include <thread>
-#include <chrono>
-#include <iostream>
 #include "solve/solve.h"
 
 int main() {
-    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-//
-//    std::thread day1(solve_day_1);
-//    std::thread day2(solve_day_2);
-//    std::thread day3(solve_day_3);
+
+    std::thread day1(solve_day_1);
+    std::thread day2(solve_day_2);
+    std::thread day3(solve_day_3);
     std::thread day5 (solve_day_5);
 
-//    day1.join();
-//    day2.join();
-//    day3.join();
+    day1.join();
+    day2.join();
+    day3.join();
     day5.join();
-
-    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-    std::cout << duration << "ms " << std::endl;
 
 }
